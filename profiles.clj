78cfc3587096b8cc147d7dbbd1734caf10bcd806
Roberto Acevedo <rxacevedo@gmail.com>
2015-02-28 10:50:17 -0500

    Initial commit

---
+++ profiles.clj
@@ -0,0 +1,13 @@
+{:user {:local-repo "/Users/Roberto/.m2/repository/"        
+        :search-page-size 10
+        :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  [lein-exec "0.3.4"]
+                  [lein-pprint "1.1.1"]
+                  [cloc "0.1.0"]]
+        :dependencies [[clj-http "1.0.0"]
+                       [hickory "0.5.4"]
+                       [slamhound "1.5.5"]
+                       [com.cemerick/pomegranate "0.3.0"]
+                       [org.clojure/tools.nrepl "0.2.7"]]}
+ :analysis {:dependencies [[incanter "1.5.6"]]}
+ :scraping {:dependencies [[com.github.kyleburton/clj-xpath "1.4.3"]]}}

1eef92d2da35d9b555ec0cb073a36343fefab9b3
Roberto Acevedo <rxacevedo@users.noreply.github.com>
2015-05-10 18:48:10 -0400

    Update for Linux and clean up
    Change to Linux home for local Maven repo, remove unused plugins and update dependency versions.

--- profiles.clj
+++ profiles.clj
@@ -1,13 +1,11 @@
-{:user {:local-repo "/Users/Roberto/.m2/repository/"        
+{:user {:local-repo "/home/roberto/.m2/repository/"        
         :search-page-size 10
         :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [lein-exec "0.3.4"]
-                  [lein-pprint "1.1.1"]
-                  [cloc "0.1.0"]]
-        :dependencies [[clj-http "1.0.0"]
+                  [lein-exec "0.3.4]"
+        :dependencies [[clj-http "1.1.2"]
                        [hickory "0.5.4"]
                        [slamhound "1.5.5"]
                        [com.cemerick/pomegranate "0.3.0"]
                        [org.clojure/tools.nrepl "0.2.7"]]}
- :analysis {:dependencies [[incanter "1.5.6"]]}
- :scraping {:dependencies [[com.github.kyleburton/clj-xpath "1.4.3"]]}}
+ :analysis {:dependencies [[incanter "1.6.0"]]}
+ :scraping {:dependencies [[com.github.kyleburton/clj-xpath "1.4.4"]]}}

9d813d1e420eeb904dbbab127f52ccab57183f60
Roberto Acevedo <rxacevedo@users.noreply.github.com>
2015-05-10 19:02:05 -0400

    Fix syntex error

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,6 @@
 {:user {:search-page-size 10
         :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [lein-exec "0.3.4]"
+                  [lein-exec "0.3.4"]]
         :dependencies [[clj-http "1.1.2"]
                        [hickory "0.5.4"]
                        [slamhound "1.5.5"]

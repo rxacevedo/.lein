b3cb3e830f04cbd37e165665593c2dc4d431654f
Roberto Acevedo <rxacevedo@fastmail.com>
2015-06-30 09:42:30 -0400

    Update profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,13 +1,12 @@
-{:user {:local-repo "/Users/Roberto/.m2/repository/"        
+{:user {:aliases {"slamhound" ["run" "-m" "slam.hound"]}
+        :local-repo "/Users/Roberto/.m2/repository/"
         :search-page-size 10
-        :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [lein-exec "0.3.4"]
-                  [lein-pprint "1.1.1"]
-                  [cloc "0.1.0"]]
-        :dependencies [[clj-http "1.0.0"]
+        :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                  [lein-instant-cheatsheet "2.1.3"]
+                  [lein-exec "0.3.4"]]
+        :dependencies [[clj-http "1.1.0"]
                        [hickory "0.5.4"]
                        [slamhound "1.5.5"]
-                       [com.cemerick/pomegranate "0.3.0"]
-                       [org.clojure/tools.nrepl "0.2.7"]]}
+                       [com.cemerick/pomegranate "0.3.0"]]}
  :analysis {:dependencies [[incanter "1.5.6"]]}
- :scraping {:dependencies [[com.github.kyleburton/clj-xpath "1.4.3"]]}}
+ :scraping {:dependencies [[com.github.kyleburton/clj-xpath "1.4.4"]]}}
